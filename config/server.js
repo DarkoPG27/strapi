7c32c3ba6dae6d0f2dd51ae0cd5cdeb312a7cc03
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-02-19 13:55:53 +0100

    strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1ad6274fd9d4749ae2ceb9cb24b12541'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9c736d4-e334-4a31-8456-f594f9d81bc8'
+};
